d02875553eb6b4f65ee1b4b62d5a1a02033948a2
Evan <oicleevan@protonmail.com>
2021-09-03 17:28:17 -0400

    more secure and safe index.js

--- index.js
+++ index.js
@@ -4,13 +4,20 @@
 	-- oicleevan
 */
 
-const tmi = require('tmi.js');
-const say = require('say');
-const process = require('process');
+const tmi = require('tmi.js')
+const say = require('say')
+const process = require('process')
 
-const {prefix, favourite_emote, username} = require('./config.json');
+const {prefix, favourite_emote, username} = require('./config.json')
 
-const oauth = process.argv[2];
+var oauth
+
+if(process.argv[2] != null) {
+	oauth = process.argv[2]
+} else {
+	console.log("Please include an oauth password.")
+	process.exit()
+}
 
 const config = {
 	options: {
@@ -20,44 +27,44 @@ const config = {
 		reconnect: true
 	},
 	identity: {
-		username: username,
-		password: oauth // your login. for purposes of safety on my end, currently blank. 
+		username: username, // your username, configured in config.json.
+		password: oauth // your login password. for purposes of security, must be entered at runtime. 
 	},
 	channels: ["stylislol"] // channels that this bot is enabled in.
-};
+}
 
-const client = new tmi.client(config);
-client.connect(); 
+const client = new tmi.client(config)
+client.connect()
 
 client.on("connected", (address, port) => {
-    console.log(`connected to ${address} on port ${port}`);
-});
+    console.log(`connected to ${address} on port ${port}`)
+})
 
 client.on("message", function (channel, userstate, message, self) {
 	
 	// i had a bunch of if statements, but i think this is more elegant.
 	switch(message.toLowerCase()) {
 		case `${prefix}about`:
-			client.say(channel, "want to know more about the project? read at https://oicleevan.github.io/stylisbot/about.html");
-			break;
+			client.say(channel, "want to know more about the project? read at https://oicleevan.github.io/stylisbot/about.html")
+			break
 		case `${prefix}commands`:
-			client.say(channel, "a list of commands are available at https://oicleevan.github.io/stylisbot/commands.html");
-			break;
+			client.say(channel, "a list of commands are available at https://oicleevan.github.io/stylisbot/commands.html")
+			break
 		case `${prefix}emote`:
-			client.say(channel, `my favourite emote right now is...  ${favourite_emote} !`);
-			break;
+			client.say(channel, `my favourite emote right now is...  ${favourite_emote} !`)
+			break
 		case `${prefix}vanish`:
-			client.timeout(channel, userstate.username, 1, "vanish").catch();
-			break;
+			client.timeout(channel, userstate.username, 1, "vanish").catch()
+			break
 		default:
-			break;
+			break
 	}
 
 	// because tts includes more than just the message, i had to exclude it from the switch
 	if(message.toLowerCase().includes(`${prefix}say`)) {
-		var tts = message.replace(`${prefix}say `, "");
-		say.speak(`${userstate.username} said ${tts}`);
+		var tts = message.replace(`${prefix}say `, "")
+		say.speak(`${userstate.username} said ${tts}`)
 	}
 
-	console.log(`[${channel} | ${userstate.username}] ${message}`);
-});
+	console.log(`[${channel} | ${userstate.username}] ${message}`)
+})
